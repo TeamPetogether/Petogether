5eef4a73dda1d7671c9dfcc11f25bcd9be612ba7
Seongeun Jo <168450770+Seongeun-Jo@users.noreply.github.com>
2025-06-04 22:55:24 +0900

    이전 내역 확인 페이지를 위한 models.py 수정

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from .database import Base
+from datetime import datetime
 
 class User(Base):
     __tablename__ = "users"
@@ -10,6 +11,7 @@ class User(Base):
     password = Column(String(100), nullable=False)
 
 
+
 class Note(Base):
     __tablename__ = "notes"
 
@@ -17,3 +19,37 @@ class Note(Base):
     date = Column(String(255))  
     note = Column(String(1000))  
     image_path = Column(String(1000)) 
+
+class Walk(Base):
+    __tablename__ = "walks"
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(String(20), index=True)
+    walk_type = Column(String(100))
+    walk_path = Column(String(255))
+    walk_duration = Column(String(50))
+    memo = Column(String(1000))
+
+class DogBreed(Base):
+    __tablename__ = "dog_breeds"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), unique=True, nullable=False)
+    walk_count = Column(String(50))  # 예: '1~2회/일'
+    walk_duration = Column(String(50))  # 예: '15~30분/회'
+    health_warning = Column(String(255))  # 예: '슬개골탈구'
+    notes = Column(String(255))  # 기타 특이사항
+
+class UserDog(Base):
+    __tablename__ = "user_dogs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    dog_breed_id = Column(Integer, ForeignKey("dog_breeds.id"))
+
+class CheckHistory(Base):
+    __tablename__ = "check_history"
+
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(DateTime, default=datetime.utcnow)
+    message = Column(String(255))
+    note = Column(String(500))
